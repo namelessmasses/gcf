b19dac002149b3b27b0dfce465c174b6ec2a435e
namelessmasses <ngarimu@gmail.com>
2017-12-03 23:44:37 -0800

    - sender.py: add comments
    - Comments describing changes required for the python3 interfaces between str vs. bytes.

--- sender.py
+++ sender.py
@@ -9,6 +9,9 @@ import time
 
 publish_url = 'https://cloudiot-device.googleapis.com/v1beta1/projects/plasma-sol-181606/registries/ngarimu-reg-0/devices/device0:publishEvent'
 
+# Note that python3 has different interfaces between string and bytes
+# The result of create_jwt() is binary bytes and must be decoded back to 
+# a UTF-8 string fro the token.
 jwt = create_jwt('plasma-sol-181606', 'device0/rsa_private_device0.pem', 'RS256').decode('utf-8')
 
 headers = {
@@ -27,6 +30,10 @@ try:
         if payload != oldpin:
             oldpin = payload
             print(payload)
+            # Just like the JWT a string must be endcoded to bytes before it can 
+            # be Base64 encoded (b64encode() takes a bytes like object not a string).
+            # Similarly, the result of the Base64 encode must be decode('UTF-8') back to
+            # string for JSON to work (seems binary strings are not JSON serializable).
             body = {
                 'binary_data': base64.urlsafe_b64encode('{}'.format(payload).encode('utf-8')).decode('utf-8')
             }
